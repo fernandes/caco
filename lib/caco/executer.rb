08e664da458bea11db3ea21a9d3fe442bf47c35d
Celso Fernandes <celso.fernandes@gmail.com>
2019-11-26 10:48:13 -0300

    Improve Caco::Executer to return stderr

--- lib/caco/executer.rb
+++ lib/caco/executer.rb
@@ -5,10 +5,11 @@ module Caco
     step :execute!
 
     def execute!(ctx, command:, **)
-      s, e, o = self.class.send(:execute, command)
-      ctx[:signal] = [s, e, o]
+      s, e, o, error = self.class.send(:execute, command)
+      ctx[:signal] = [s, e, o, error]
       ctx[:exit_code] = e
       ctx[:output] = o
+      ctx[:stderr] = error
 
       return s
     end
@@ -16,15 +17,17 @@ module Caco
     module ClassMethods
       private
       def execute(command)
-        out = nil
+        stdout = nil
+        stderr = nil
         pid = nil
         exit_status = nil
         Open3.popen3(*command) do |i, o, e, t|
           pid = t.pid
-          out = o.read
+          stdout = o.read
+          stderr = e.read
           exit_status = t.value
         end
-        return exit_status.success?, exit_status.exitstatus, out
+        return exit_status.success?, exit_status.exitstatus, stdout, stderr
       end
     end
     extend ClassMethods
--- test/caco/executer_test.rb
+++ test/caco/executer_test.rb
@@ -24,13 +24,13 @@ class Caco::ExecuterTest < Minitest::Test
   def test_multiple_stubs
     @commander = Minitest::Mock.new
     @commander.expect :call, [true, 0, "out 1"], ['command_1']
-    @commander.expect :call, [false, 1, "out 2"], ['command_2']
+    @commander.expect :call, [false, 1, "out 2", "stderror"], ['command_2']
     @commander.expect :call, [true, 0, "out 3"], ['command_3']
 
     described_class.stub :execute, ->(command){ @commander.call(command) } do
-      assert_equal [true, 0, "out 1"], described_class.(params: { command: "command_1" })[:signal]
-      assert_equal [false, 1, "out 2"], described_class.(params: { command: "command_2" })[:signal]
-      assert_equal [true, 0, "out 3"], described_class.(params: { command: "command_3" })[:signal]
+      assert_equal [true, 0, "out 1", nil], described_class.(params: { command: "command_1" })[:signal]
+      assert_equal [false, 1, "out 2", "stderror"], described_class.(params: { command: "command_2" })[:signal]
+      assert_equal [true, 0, "out 3", nil], described_class.(params: { command: "command_3" })[:signal]
     end
 
     @commander.verify
