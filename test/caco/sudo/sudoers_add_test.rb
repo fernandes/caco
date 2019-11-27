d6bb4b9ef2211e56b43d8d944fe3c8544c0598eb
Celso Fernandes <celso.fernandes@gmail.com>
2019-11-27 17:43:01 -0300

    Fix sudoers

--- test/caco/sudo/sudoers_add_test.rb
+++ test/caco/sudo/sudoers_add_test.rb
@@ -1,25 +1,5 @@
 require "test_helper"
 
-module Caco::Sudo
-  class SudoersAdd < Trailblazer::Operation
-    step Caco::Macro::ValidateParamPresence(:identifier)
-    step Caco::Macro::ValidateParamPresence(:content)
-    step Caco::Macro::NormalizeParams()
-    step :build_path
-    step Subprocess(Caco::FileWriter),
-    input:  ->(_ctx, path:, content:, **) do { params: {
-      path: path,
-      content: content
-    } } end,
-    output: {file_created: :created, file_changed: :changed}
-
-
-    def build_path(ctx, identifier:, **)
-      ctx[:path] = "/etc/sudoers.d/#{identifier}"
-    end
-  end
-end
-
 class Caco::Sudo::SudoersAddTest < Minitest::Test
   def setup
     clean_tmp_path
