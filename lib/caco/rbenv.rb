b376f220f14fed25288593040adfa03d6b011851
Celso Fernandes <celso.fernandes@gmail.com>
2023-05-21 17:56:33 -0300

    Rbenv

--- lib/caco/rbenv.rb
+++ lib/caco/rbenv.rb
@@ -1,5 +1,155 @@
-module Caco
-  module Rbenv
+# frozen_string_literal: true
+# typed: strict
+
+module Caco::Rbenv
+  # @@root = "/opt/rbenv"
+  mattr_accessor :root
+
+  extend T::Sig
+
+  PACKAGES_TO_INSTALL = T.let({
+    'stretch': %w[
+      git autoconf bison libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev
+      libffi-dev libgdbm3 libgdbm-dev
+    ],
+    'buster': %w[
+      git autoconf bison libssl-dev libyaml-dev libreadline-dev zlib1g-dev
+      libncurses5-dev libffi-dev libgdbm6 libgdbm-dev
+    ],
+    'bullseye': %w[
+      autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev
+      zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+    ]
+  }.freeze, T::Hash[Symbol, T::Array[String]])
+
+  sig { returns(Caco::Resource::Result) }
+  def self.install
+    install_dependencies
+    clone_repo
+    write_profile_file
+  end
+
+  sig { returns(T::Boolean) }
+  def self.repo_exist?
+    File.exist?(install_to)
+  end
+
+  sig { returns(T::Boolean) }
+  def self.clone_repo
+    return true if repo_exist?
+
+    Caco.execute('clone rbenv repo', command: "git clone https://github.com/rbenv/rbenv.git #{install_to}").success?
+  end
+
+  sig { returns(Caco::Resource::Result) }
+  def self.write_profile_file
+    profile_content = Caco::Rbenv::Cell::Profile.call.to_s
+    Caco.file '/etc/profile.d/rbenv.sh', content: profile_content
+  end
+
+  sig { params(version: String).returns(T::Boolean) }
+  def self.version_installed?(version)
+    has_version = rbenv_command(
+      "rbenv_version_installed? #{version}",
+      command: "rbenv versions|egrep -q \"#{version}\""
+    )
+    has_version.success?
+  end
+
+  sig { params(version: String).returns(T::Boolean) }
+  def self.install_version(version)
+    return true if version_installed?(version)
+
+    result = rbenv_command("rbenv_install_version #{version}", command: "rbenv install #{version}")
+    result.success?
+  end
+
+  sig { returns(String) }
+  def self.global_version
+    result = rbenv_command('rbenv_global_version', command: 'rbenv global')
+    T.cast(T.cast(result.output, String).chomp!, String)
+  end
+
+  sig { params(version: String).returns(T::Boolean) }
+  def self.global_version=(version)
+    return true if global_version == version
+
+    result = rbenv_command("rbenv_set_global_version #{version}", command: "rbenv global #{version}")
+    result.success?
+  end
+
+  sig { params(name: String, command: String).returns(Caco::Resource::Result) }
+  def self.rbenv_command(name, command:)
+    Caco.execute name, command: <<~CMD
+      export PATH="#{install_to}/bin:${PATH}" ;
+      export RBENV_ROOT="#{install_to}" ;
+      eval "$(rbenv init -)";
+      #{command}
+    CMD
+  end
+
+  sig { returns(T.any(TrueClass, Caco::Resource::Result)) }
+  def self.install_ruby_build
+    return true if File.exist?("#{install_to}/plugin/ruby-build")
+
+    Caco.execute(
+      'clone ruby-build repo',
+      command: "git clone https://github.com/rbenv/ruby-build.git #{install_to}/plugins/ruby-build"
+    )
+  end
+
+  sig { returns(T.any(TrueClass, Caco::Resource::Result)) }
+  def self.install_rbenv_each
+    return true if File.exist?("#{install_to}/plugin/rbenv-each")
+
+    Caco.execute(
+      'clone rbenv-each repo',
+      command: "git clone https://github.com/rbenv/rbenv-each.git #{install_to}/plugins/rbenv-each"
+    )
+  end
+
+  sig { returns(T::Boolean) }
+  def self.install_rbenv_default_gems
+    unless File.exist?("#{install_to}/plugins/rbenv-default-gems")
+      Caco.execute(
+        'clone rbenv-default-gems repo',
+        command: "git clone https://github.com/rbenv/rbenv-default-gems.git #{install_to}/plugins/rbenv-default-gems"
+      )
+    end
+
+    Caco.file "#{install_to}/default-gems", content: <<~GEMS
+      bundler
+      foreman
+      tmuxinator
+      mina
+    GEMS
+
+    true
+  end
+
+  sig { returns(Caco::Resource::Result) }
+  def self.update_ruby_build
+    Caco.execute(
+      'update ruby-build repo',
+      command: "git -C #{install_to}/plugins/ruby-build pull"
+    )
+  end
+
+  sig { params(distro_name: Symbol).returns(T::Array[String]) }
+  def self.packages_to_install(distro_name)
+    PACKAGES_TO_INSTALL.fetch(distro_name, [])
+  end
+
+  sig { returns(T::Array[String]) }
+  def self.install_dependencies
+    distro_name = Caco::Facter.call('os', 'distro', 'codename')
+    packages_to_install(distro_name.to_sym).each do |package|
+      Caco.package package
+    end
+  end
+
+  sig { returns(String) }
+  def self.install_to
+    root || '/opt/rbenv'
   end
 end
-
---
+++ test/caco/rbenv_test.rb
@@ -0,0 +1,172 @@
+require "test_helper"
+
+class Caco::RbenvTest < Minitest::Test
+
+  def setup
+    clean_tmp_path
+    FileUtils.mkdir_p(TMP_PATH)
+    Caco::Rbenv.root = "#{TMP_PATH}/opt/rbenv"
+  end
+
+  def teardown
+    Caco::Rbenv.root = '/opt/rbenv'
+  end
+
+  class Install < Caco::RbenvTest
+    # This test is like a remined if I need to run a sequence of commandss
+    def test_install
+      returns = stub_for_dependencies
+      returns << [[true, 0, ""], ["git clone https://github.com/rbenv/rbenv.git /home/fernandes/config/caco/test/tmp/opt/rbenv"]]
+
+      executer_stub(returns) do
+        Caco::Rbenv.install
+      end
+
+      assert_equal File.read("#{TMP_PATH}/etc/profile.d/rbenv.sh"), profile_content
+    end
+  end
+
+  class VersionInstalledTest < Caco::RbenvTest
+    def test_for_new_one
+      returns = [
+        [[false, 1, ""], [rbenv_command("rbenv versions|egrep -q \"2.7.6\"")]]
+      ]
+
+      executer_stub(returns) do
+        refute Caco::Rbenv.version_installed?("2.7.6")
+      end
+    end
+
+    def test_for_existing_one
+      returns = [
+        [[true, 0, ""], [rbenv_command("rbenv versions|egrep -q \"2.7.5\"")]]
+      ]
+
+      executer_stub(returns) do
+        assert Caco::Rbenv.version_installed?("2.7.5")
+      end
+    end
+  end
+
+  class InstallVersion < Caco::RbenvTest
+    def test_install_version
+      returns = [
+        [[false, 1, ""], [rbenv_command("rbenv versions|egrep -q \"2.7.5\"")]],
+        [[true, 0, ""], [rbenv_command("rbenv install 2.7.5")]]
+      ]
+
+      executer_stub(returns) do
+       Caco::Rbenv.install_version("2.7.5")
+      end
+    end
+  end
+
+  class GlobalVersion < Caco::RbenvTest
+    def test_global_version
+      returns = [
+        [[true, 0, "2.7.5\n"], [rbenv_command("rbenv global")]]
+      ]
+
+      executer_stub(returns) do
+        assert_equal "2.7.5", Caco::Rbenv.global_version
+      end
+    end
+
+    def test_set_global_version
+      returns = [
+        [[true, 0, "2.7.5\n"], [rbenv_command("rbenv global")]],
+        [[true, 0, ""], [rbenv_command("rbenv global 2.7.6")]]
+      ]
+
+      executer_stub(returns) do
+        assert Caco::Rbenv.global_version = '2.7.6'
+      end
+    end
+
+    def test_set_global_version_idempotent
+      returns = [
+        [[true, 0, "2.7.6\n"], [rbenv_command("rbenv global")]],
+      ]
+
+      executer_stub(returns) do
+        assert Caco::Rbenv.global_version = '2.7.6'
+      end
+    end
+  end
+
+  class RbenvCommandTest < Caco::RbenvTest
+    def test_run_command
+      command = <<~COMMAND
+        export PATH="#{Caco::Rbenv.install_to}/bin:${PATH}" ;
+        export RBENV_ROOT="#{Caco::Rbenv.install_to}" ;
+        eval "$(rbenv init -)";
+        rbenv foo
+      COMMAND
+
+      returns = [
+        [[true, 0, "2.7.5\n"], [command]]
+      ]
+
+      executer_stub(returns) do
+      Caco::Rbenv.rbenv_command("foo", command: "rbenv foo")
+      end
+    end
+  end
+
+  class InstallRubyBuildTest < Caco::RbenvTest
+    def test_install_ruby_build
+      returns = [
+        [[true, 0, ""], ["git clone https://github.com/rbenv/ruby-build.git #{Caco::Rbenv.install_to}/plugins/ruby-build"]]
+      ]
+
+      File.stub :exist?, false do
+        executer_stub(returns) do
+          Caco::Rbenv.install_ruby_build
+        end
+      end
+    end
+  end
+
+  class InstallRbenvEachTest < Caco::RbenvTest
+    def test_install_rbenv_each
+      returns = [
+        [[true, 0, ""], ["git clone https://github.com/rbenv/rbenv-each.git #{Caco::Rbenv.install_to}/plugins/rbenv-each"]]
+      ]
+
+      File.stub :exist?, false do
+        executer_stub(returns) do
+          Caco::Rbenv.install_rbenv_each
+        end
+      end
+    end
+  end
+
+  def stub_for_dependencies
+    returns = []
+    %w[
+      git autoconf bison libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev
+      libffi-dev libgdbm3 libgdbm-dev
+    ].each do |pkg|
+      returns << [[false, 1, ""], ["dpkg -s #{pkg}"]]
+      returns << [[true, 0, ""], ["apt-get install -y #{pkg}"]]
+    end
+    returns
+  end
+
+  def rbenv_command(command)
+    <<~COMMAND
+      export PATH="#{Caco::Rbenv.install_to}/bin:${PATH}" ;
+      export RBENV_ROOT="#{Caco::Rbenv.install_to}" ;
+      eval "$(rbenv init -)";
+      #{command}
+    COMMAND
+  end
+
+  def profile_content
+  <<~CONTENT
+    export PATH="/opt/rbenv/bin:$PATH"
+    export RBENV_ROOT="/opt/rbenv"
+    eval "$(rbenv init -)"
+  CONTENT
+  end
+end
